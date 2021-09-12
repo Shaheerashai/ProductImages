ed274d1b7f25b7fc1e5659dec8fb5d466e1500b0
Shai <shai1232009@gmail.com>
2021-09-12 16:27:35 +0200

    timer added

--- script.js
+++ script.js
@@ -30,3 +30,33 @@ var swiper = new Swiper(".mySwiper", {
         invert: true,
       },
 });
+(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let sale = "Sep 13, 2021 00:00:00",
+        countDown = new Date(sale).getTime(),
+        x = setInterval(function () {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            if (distance < 0) {
+                headline = document.getElementById("headline");
+                countdown = document.getElementById("countdown");
+                countdown.style.display = "none";
+                headline.style.display = "none";
+
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
+}());
